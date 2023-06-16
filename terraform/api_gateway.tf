9fcc84c1bf2cb698e938d0754141d9d1b6919a00
Rohin Chopra <rohinchopra1212@gmail.com>
2023-06-16 21:59:24 +1000

    renamed some resources

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -1,4 +1,4 @@
-resource "aws_api_gateway_rest_api" "personal_website_api" {
+resource "aws_api_gateway_rest_api" "api" {
   name           = "personal-website-api"
   api_key_source = "HEADER"
 }
@@ -7,61 +7,61 @@ resource "aws_lambda_permission" "contact_lambda_api_gateway_permission" {
   function_name = local.contact_lambda_function_name
   principal     = "apigateway.amazonaws.com"
   action        = "lambda:InvokeFunction"
-  source_arn    = "${aws_api_gateway_rest_api.personal_website_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
 
   depends_on = [aws_lambda_function.contact_lambda]
 }
 
-resource "aws_api_gateway_resource" "personal_website_api_contact_resource" {
-  rest_api_id = aws_api_gateway_rest_api.personal_website_api.id
-  parent_id   = aws_api_gateway_rest_api.personal_website_api.root_resource_id
+resource "aws_api_gateway_resource" "api_contact_resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   path_part   = "contact"
 }
 
-resource "aws_api_gateway_method" "personal_website_api_contact_method" {
-  rest_api_id      = aws_api_gateway_rest_api.personal_website_api.id
-  resource_id      = aws_api_gateway_resource.personal_website_api_contact_resource.id
+resource "aws_api_gateway_method" "api_contact_method" {
+  rest_api_id      = aws_api_gateway_rest_api.api.id
+  resource_id      = aws_api_gateway_resource.api_contact_resource.id
   http_method      = "POST"
   authorization    = "NONE"
   api_key_required = true
 }
 
-resource "aws_api_gateway_integration" "personal_website_api_lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.personal_website_api.id
-  resource_id             = aws_api_gateway_resource.personal_website_api_contact_resource.id
-  http_method             = aws_api_gateway_method.personal_website_api_contact_method.http_method
+resource "aws_api_gateway_integration" "api_lambda_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_contact_resource.id
+  http_method             = aws_api_gateway_method.api_contact_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.contact_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "personal_website_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.personal_website_api.id
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
   stage_name  = "dev"
 
-  depends_on = [aws_api_gateway_integration.personal_website_api_lambda_integration]
+  depends_on = [aws_api_gateway_integration.api_lambda_integration]
 }
 
-resource "aws_api_gateway_usage_plan" "personal_website_api_usage_plan" {
+resource "aws_api_gateway_usage_plan" "api_usage_plan" {
   name = "personal-website-api-usage-plan"
 
   api_stages {
-    api_id = aws_api_gateway_rest_api.personal_website_api.id
+    api_id = aws_api_gateway_rest_api.api.id
     stage  = "dev"
   }
 }
 
-resource "aws_api_gateway_api_key" "personal_website_api_key" {
+resource "aws_api_gateway_api_key" "api_key" {
   name = "personal-website-api-key"
 }
 
 resource "aws_api_gateway_usage_plan_key" "usage_plan_key_notification" {
-  key_id        = aws_api_gateway_api_key.personal_website_api_key.id
+  key_id        = aws_api_gateway_api_key.api_key.id
   key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.personal_website_api_usage_plan.id
+  usage_plan_id = aws_api_gateway_usage_plan.api_usage_plan.id
 }
 
-resource "aws_api_gateway_domain_name" "personal_website_api_domain" {
+resource "aws_api_gateway_domain_name" "api_domain_name" {
   domain_name              = "api.rohinchopra.com"
   regional_certificate_arn = aws_acm_certificate_validation.api_certificate_validation.certificate_arn
   security_policy          = "TLS_1_2"
@@ -71,10 +71,10 @@ resource "aws_api_gateway_domain_name" "personal_website_api_domain" {
   }
 }
 
-resource "aws_api_gateway_base_path_mapping" "personal_website_api_domain_connection" {
+resource "aws_api_gateway_base_path_mapping" "api_base_path_mapping" {
   domain_name = aws_acm_certificate.certificate.domain_name
-  api_id      = aws_api_gateway_rest_api.personal_website_api.id
+  api_id      = aws_api_gateway_rest_api.api.id
   stage_name  = "dev"
 
-  depends_on = [aws_api_gateway_domain_name.personal_website_api_domain]
+  depends_on = [aws_api_gateway_domain_name.api_domain_name]
 }
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -26,8 +26,8 @@ resource "aws_route53_record" "api_record" {
   zone_id = data.aws_route53_zone.primary_zone.zone_id
 
   alias {
-    name                   = aws_api_gateway_domain_name.personal_website_api_domain.regional_domain_name
-    zone_id                = aws_api_gateway_domain_name.personal_website_api_domain.regional_zone_id
+    name                   = aws_api_gateway_domain_name.api_domain_name.regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.api_domain_name.regional_zone_id
     evaluate_target_health = false
   }
 }
